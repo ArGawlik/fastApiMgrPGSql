07f5eca7d52f6e769259719c9fb3c8e263052ddb
Arkadiusz Gawlik <96ar.gaw@gmail.com>
2022-09-02 21:23:59 +0200

    db change

--- app/database.py
+++ app/database.py
@@ -3,10 +3,13 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:DaftAcademy@127.0.0.1:5555/postgres"
+# SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:DaftAcademy@127.0.0.1:5555/postgres"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine("sqlite:///C:\\Users\\Beryl\\Desktop\\python\\fastApiMgrPGSql\\northwind.db?check_same_thread=False&unicode_error=ignore")
+
+# engine.raw_connection().connection.text_factory = lambda b: b.decode(errors="ignore", encoding="utf8")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
