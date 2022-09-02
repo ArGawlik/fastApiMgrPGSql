7f89d2e29461daf2b85b7e4b3de74adcb002d717
Arkadiusz Gawlik <96ar.gaw@gmail.com>
2022-09-02 21:34:28 +0200

    db change

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:DaftAcademy@127.0.0.1:5555/postgres"
 
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
-engine = create_engine("sqlite:///C:\\Users\\Beryl\\Desktop\\python\\fastApiMgrPGSql\\northwind.db?check_same_thread=False&unicode_error=ignore")
+engine = create_engine("sqlite:///northwind.db?check_same_thread=False&unicode_error=ignore")
 
 # engine.raw_connection().connection.text_factory = lambda b: b.decode(errors="ignore", encoding="utf8")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
